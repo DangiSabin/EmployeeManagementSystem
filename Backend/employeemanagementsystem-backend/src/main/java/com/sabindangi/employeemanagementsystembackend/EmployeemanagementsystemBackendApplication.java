635f578d9b8d5443d97db87bbe459d42fd4b0ad0
sabin dangi <sabin4.dangi@gmail.com>
2021-05-05 18:29:53 -0600

    employeemanagementsystem-backend

---
+++ Backend/employeemanagementsystem-backend/src/main/java/com/sabindangi/employeemanagementsystembackend/EmployeemanagementsystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sabindangi.employeemanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagementsystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagementsystemBackendApplication.class, args);
+	}
+
+}
